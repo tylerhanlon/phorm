b85e80083c1d24e09e6b70b8cc877629d267d1bd
tylerhanlon <54967903+tylerhanlon@users.noreply.github.com>
2023-03-18 14:43:50 -0400

    updates to match cpanel after AJAX

--- scripts/signup_page_input_query.js
+++ scripts/signup_page_input_query.js
@@ -5,7 +5,16 @@ function Customer (fname, lname, em, ph) {
     this.phone = ph;
 }
 
-const form = $("#signup-page-form").submit(function() {
-    customer = new Customer($("#fname").val(), $("#lname").val(), $("#email").val(), $("#phone").val());
-    alert("Thank you for providing your contact information. Here is the information we have received: \n" + customer.firstName + '\n' + customer.lastName + '\n' + customer.email + '\n' + customer.phone);
-})
+var $form = $("#signup-page-form")
+$form.addEventListener('submit', submitForm);
+
+function submitForm(event){
+    event.preventDefault();
+    customer = new Customer($("#fname").val(), $("#lname").val(), $("#email").val(), $("#phone").val());    
+}
+
+// const form = $("#signup-page-form").submit(function() {
+//     event.preventDefault();
+//     customer = new Customer($("#fname").val(), $("#lname").val(), $("#email").val(), $("#phone").val());
+//     alert("Thank you for providing your contact information. Here is the information we have received: \n" + customer.firstName + '\n' + customer.lastName + '\n' + customer.email + '\n' + customer.phone);
+// })
---
+++ scripts/trainer_summary_html.js
@@ -0,0 +1,68 @@
+function ajaxRequestHTML(htmlPath){
+    var xhr = new XMLHttpRequest();
+    //onLoad function
+    console.log(htmlPath);
+    xhr.onload = function(){
+        if(xhr.status === 200){
+            document.getElementsByClassName("trainer-summary")[0].innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open("GET", htmlPath, true);
+    xhr.send(null);
+}
+
+$(document).ready(function(){
+    var current_image = 0;
+    const images = $('.trainers-row');
+    const pics = $('.trainer-pic')
+    //addNewElement();
+    $("#prev-btn").click(function() {
+       current_image--;
+       if(current_image < 0){
+           current_image = pics.length - 1;
+       }
+       images.css("transitionDuration", "0.5s");
+       images.css("transform", `translate(-${current_image * 19.5}vw)`);
+       update_trainer_info();
+    })
+    
+    $("#next-btn").click(function() {
+        current_image++;
+        if(current_image >= pics.length){
+            current_image = 0;
+        }
+        images.css('transitionDuration', "0.5s");
+        images.css("transform", `translate(-${current_image * 19.5}vw)`);
+        update_trainer_info();
+    })
+    
+    function addNewElement(){
+        $('.trainer-summary').append('<p>Sign up to schedule a call with one of our trainers!</p>')
+    }
+    
+    function changeBorder(){
+        $('.trainer-pic').css("border", "1px solid gold");
+    }
+    
+    function update_trainer_info() {
+    
+        switch(current_image) {
+            case 0:
+                ajaxRequestHTML("/phorm_v1/taiSummary.html");
+                break;
+            case 1:
+                ajaxRequestHTML("/phorm_v1/morganSummary.html");
+                break;
+            case 2:
+                ajaxRequestHTML("/phorm_v1/amandaSummary.html");
+                break;
+            case 3:
+                ajaxRequestHTML("/phorm_v1/peterSummary.html");
+                break;
+            default:
+                ajaxRequestHTML("/phorm_v1/taiSummary.html");
+          }
+    }
+    
+    
+})
